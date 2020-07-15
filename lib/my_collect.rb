fecc65fdb0ae07dd8c3557f72e7027397976f8e0
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-15 00:15:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  
+  i = 0 
+  new_collection = []
+  
+  while i < collection.size 
+    new_collection << yield(collection[i])
+    i += 1 
+  end
+  
+  new_collection
 
-
+end
